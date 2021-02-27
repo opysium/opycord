486e08bd529fff045d0d4e16302a2d9ed3dbdca2
opysium <73075946+opysium@users.noreply.github.com>
2021-02-27 23:40:45 +0300

    Updated to make getting higher values harder.

--- src/modules/commandHandler/commands/kaçcm.js
+++ src/modules/commandHandler/commands/kaçcm.js
@@ -2,19 +2,13 @@ module.exports = {
     name: 'kaçcm',
     execute(message) {
         let length;
-        switch(Math.floor(Math.random() * Math.floor(3))) {
-            case 0: {
-                length = Math.floor(Math.random() * Math.floor(25));
-                break;
-            }
-            case 1: {
-                length = Math.floor(Math.random() * Math.floor(35));
-                break;
-            }
-            case 2: {
-                length = Math.floor(Math.random() * Math.floor(50));
-                break;
-            }
+        const lengthFactor = Math.floor(Math.random() * Math.floor(95));
+        if(0 <= lengthFactor <= 60) {
+            length = Math.floor(Math.random() * Math.floor(25));
+        } else if(51 <= lengthFactor <= 30) {
+            length = Math.floor(Math.random() * Math.floor(35));
+        } else if(76 <= lengthFactor <= 15) {
+            length = Math.floor(Math.random() * Math.floor(50));
         }
         if(!message.mentions.users.first()) {
             message.channel.send(`<@${message.author.id}> aletin ${length} santimetre.`);
