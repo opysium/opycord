d6cbc26536e3764f6a850a38e854ac07e3ffb08d
opysium <73075946+opysium@users.noreply.github.com>
2021-02-27 23:22:49 +0300

    Tags user who uses command.

--- src/modules/commandHandler/commands/kaçcm.js
+++ src/modules/commandHandler/commands/kaçcm.js
@@ -5,18 +5,21 @@ module.exports = {
         switch(Math.floor(Math.random() * Math.floor(3))) {
             case 0: {
                 length = Math.floor(Math.random() * Math.floor(25));
+                break;
             }
             case 1: {
                 length = Math.floor(Math.random() * Math.floor(35));
+                break;
             }
             case 2: {
                 length = Math.floor(Math.random() * Math.floor(50));
+                break;
             }
         }
         if(!message.mentions.users.first()) {
-            message.channel.send(`Aletin ${length} santimetre.`);
+            message.channel.send(`<@${message.author.id}> aletin ${length} santimetre.`);
         } else {
-            message.channel.send(`${message.mentions.users.first().username}'n aleti ${length} santimetre.`);
+            message.channel.send(`<@${message.author.id}>, ${message.mentions.users.first().username}'n aleti ${length} santimetre.`);
         }
     }
 };
