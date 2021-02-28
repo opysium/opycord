4718e0f3853784d8e0fc056743c4cd8ad2e35591
opysium <73075946+opysium@users.noreply.github.com>
2021-02-28 22:22:11 +0300

    removed wikipediaLink

--- src/modules/country/countries/example.js
+++ src/modules/country/countries/example.js
@@ -1,7 +1,6 @@
 module.exports = {
     name: 'Example Country',
     aliasNames: ['Örnek Ülke', 'EC', 'ÖÜ'],
-    wikipediaLink: 'https://wikipedia.org/wiki/Example_Country',
     flagImageLink: 'none',
     description: 'Example Country founded in 8462867 B.C. by example human.'
 }
