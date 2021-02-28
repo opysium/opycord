61fe0b39dfe3f50c5cc16dc40d989b19fe8e40a4
opysium <73075946+opysium@users.noreply.github.com>
2021-02-28 16:47:07 +0300

    nothing

--- src/modules/commandHandler/commands/kaçcm.js
+++ src/modules/commandHandler/commands/kaçcm.js
@@ -1,7 +1,19 @@
 module.exports = {
     name: 'kaçcm',
     execute(message) {
-        let length = Math.floor(Math.random() * Math.floor(50));
+        let length;
+        const lengthFactor = {
+            0: function() {
+                length = Math.floor(Math.random() * Math.floor(25));
+            },
+            1: function() {
+                length = Math.floor(Math.random() * Math.floor(37));
+            },
+            2: function() {
+                length = Math.floor(Math.random() * Math.floor(50));
+            }
+        };
+        lengthFactor[Math.floor(Math.random() * Math.floor(3))]();
         if(!message.mentions.users.first()) {
             message.channel.send(`<@${message.author.id}> aletin ${length} santimetre.`);
         } else {
