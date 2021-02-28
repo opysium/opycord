f1f0e795d00ad7aa92459229177039ca0535ed88
opysium <73075946+opysium@users.noreply.github.com>
2021-02-28 22:54:19 +0300

    remove country

--- src/modules/country/countries/example.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'Example Country',
-    aliasNames: ['Örnek Ülke', 'EC', 'ÖÜ'],
-    flagImageLink: 'none',
-    description: 'Example Country founded in 8462867 B.C. by example human.'
-}
