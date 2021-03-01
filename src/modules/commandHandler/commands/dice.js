5735d32adeaee41abfb1f52e77a4691573691885
opysium <73075946+opysium@users.noreply.github.com>
2021-03-01 20:55:17 +0300

    Added dice command with English, Turkish support.

---
+++ src/modules/commandHandler/commands/dice.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'dice',
+    aliases: ['zar'],
+    execute(message, args, commandName, client) {
+        let randomDice1 = Math.floor(Math.random() * Math.floor(6) + 1);
+        if(!args[0] || args[0] == 'single' || args[0] == 'tek') {
+            if(commandName == 'zar') {
+                message.channel.send(`<@${message.author.id}> bir zar attı ve ${randomDice1} geldi.`);
+            } else {
+                message.channel.send(`<@${message.author.id}> has roll a dice and get ${randomDice1}.`);
+            }
+        } else if(args[0] == 'double' || args[0] == 'çift') {
+            let randomDice2 = Math.floor(Math.random() * Math.floor(6) + 1);
+            if(commandName == 'zar') {
+                message.channel.send(`<@${message.author.id}> çift zar attı ve ${randomDice1} ${randomDice2} geldi.`);
+            } else {
+                message.channel.send(`<@${message.author.id}> has roll two dice and get ${randomDice1} ${randomDice2}.`);
+            }
+        } else if(args[0] == 'help' || args[0] == 'yardım') {
+            let embed;
+            if(commandName == 'zar') {
+                embed = new Discord.MessageEmbed()
+                    .setTitle('Zar')
+                    .setDescription('Tekli veya çiftli olarak zar atmanızı sağlar, herhangi bir parametre girmezseniz tek atacaktır.')
+                    .addFields({name: 'Parametreler', value: 'tek, çift, yardım'});
+                message.channel.send(embed);
+            } else {
+                embed = new Discord.MessageEmbed()
+                    .setTitle('Dice')
+                    .setDescription('Rolls one or two dice, it will roll one dice if you don\'t assign any arguments.')
+                    .addFields({name: 'Arguments', value: 'single, double, help'});
+                    message.channel.send(embed);
+            }
+        }
+    }
+}
