7add6d890ed3a17b9c4d9ab4f9d85e5c192c8b5f
opysium <73075946+opysium@users.noreply.github.com>
2021-03-03 01:14:02 +0300

    Added mysql module and environment variables.

--- src/modules/config/config.js
+++ src/modules/config/config.js
@@ -3,5 +3,9 @@ dotenv.config();
 
 module.exports = {
     token: process.env.token,
-    prefix: process.env.prefix
+    prefix: process.env.prefix,
+    sqlHostname: process.env.sqlHostname,
+    sqlUsername: process.env.sqlUsername,
+    sqlPassword: process.env.sqlPassword,
+    sqlDatabase: process.env.sqlDatabase
 };
