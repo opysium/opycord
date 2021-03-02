522387a796ebc0330e2ec6ea115db19e6e7be469
opysium <73075946+opysium@users.noreply.github.com>
2021-03-02 17:21:01 +0300

    Added avatar command

---
+++ src/modules/commandHandler/commands/avatar.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: 'avatar',
+    aliases: ['av'],
+    async execute(message, args, commandName, client) {
+        let embed;
+        if(!args[0]) {
+            embed = new Discord.MessageEmbed()
+                .setTitle('Avatar')
+                .setDescription(`<@${message.author.id}>'s avatar.`)
+                .setImage(message.author.avatarURL({size: 512}));
+            message.channel.send(embed);
+        } else if(args[0] == 'help') {
+            let embed = new Discord.MessageEmbed()
+                .setTitle('Avatar')
+                .setDescription('Avatar command sends the avatar of your or selected user. If you just write avatar command without any arguments it will send your own avatar, to select a user you can tag them in command or use their Discord ID.')
+                .addFields(
+                    {name: 'Arguments', value: 'help'}
+                )
+        } else if(args[0] && message.mentions.users.first()) {
+            let userInfo = message.mentions.users.first();
+            embed = new Discord.MessageEmbed()
+                .setTitle('Avatar')
+                .setDescription(`<@${userInfo.id}>'s avatar requested by <@${message.author.id}>`)
+                .setImage(userInfo.avatarURL({size:512}));
+            message.channel.send(embed);
+        } else if(args[0]) {
+            let targetUser = await message.guild.members.fetch(args[0]).catch(err => {});
+            if(!targetUser) {
+                message.channel.send(`put invalid id error here`);
+                return;
+            }
+            embed = new Discord.MessageEmbed()
+                .setTitle('Avatar')
+                .setDescription(`<@${targetUser.user.id}>'s avatar requested by <@${message.author.id}>`)
+                .setImage(targetUser.user.avatarURL({size: 512}));
+            message.channel.send(embed);
+        }
+    }
+}
