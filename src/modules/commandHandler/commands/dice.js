b9a1e1d9d187fddaedba4f9804afd95ffbb23837
opysium <73075946+opysium@users.noreply.github.com>
2021-03-02 17:21:13 +0300

    Updated help

--- src/modules/commandHandler/commands/dice.js
+++ src/modules/commandHandler/commands/dice.js
@@ -4,6 +4,28 @@ module.exports = {
     name: 'dice',
     aliases: ['zar'],
     execute(message, args, commandName, client) {
+        if(args[0] == 'help' || args[0] == 'yardım') {
+            let embed;
+            if(commandName == 'zar') {
+                embed = new Discord.MessageEmbed()
+                    .setTitle('Zar')
+                    .setDescription('Tekli veya çiftli olarak zar atmanızı sağlar, herhangi bir parametre girmezseniz tek atacaktır.')
+                    .addFields(
+                        {name: 'Parametreler', value: 'tek, çift, yardım', inline: true},
+                        {name: 'Diğer Adlar', value: 'dice(İngilizce)', inline: true}
+                    );
+            } else {
+                embed = new Discord.MessageEmbed()
+                    .setTitle('Dice')
+                    .setDescription('Rolls one or two dices depends on what argument you assing, it will roll one dice if you don\'t assign any arguments.')
+                    .addFields(
+                        {name: 'Arguments', value: 'single, double, help', inline: true},
+                        {name: 'Aliases', value: 'zar(Turkish)', inline: true}
+                    );
+            }
+            message.channel.send(embed);
+            return;
+        }
         let randomDice1 = Math.floor(Math.random() * Math.floor(6) + 1);
         if(!args[0] || args[0] == 'single' || args[0] == 'tek') {
             if(commandName == 'zar') {
@@ -17,21 +39,6 @@ module.exports = {
                 message.channel.send(`<@${message.author.id}> çift zar attı ve ${randomDice1} ${randomDice2} geldi.`);
             } else {
                 message.channel.send(`<@${message.author.id}> has rolled two dices and landed on ${randomDice1} ${randomDice2}.`);
-            }
-        } else if(args[0] == 'help' || args[0] == 'yardım') {
-            let embed;
-            if(commandName == 'zar') {
-                embed = new Discord.MessageEmbed()
-                    .setTitle('Zar')
-                    .setDescription('Tekli veya çiftli olarak zar atmanızı sağlar, herhangi bir parametre girmezseniz tek atacaktır.')
-                    .addFields({name: 'Parametreler', value: 'tek, çift, yardım'});
-                message.channel.send(embed);
-            } else {
-                embed = new Discord.MessageEmbed()
-                    .setTitle('Dice')
-                    .setDescription('Rolls one or two dices depends on what argument you assing, it will roll one dice if you don\'t assign any arguments.')
-                    .addFields({name: 'Arguments', value: 'single, double, help'});
-                    message.channel.send(embed);
             }
         }
     }
