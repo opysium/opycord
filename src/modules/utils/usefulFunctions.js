9ec86f5f1d784b4819e344c557b4601cd0a2da55
opysium <73075946+opysium@users.noreply.github.com>
2021-03-02 22:19:53 +0300

    Update usefulFunctions.js

--- src/modules/utils/usefulFunctions.js
+++ src/modules/utils/usefulFunctions.js
@@ -1,6 +1,8 @@
 module.exports.FindUser = async function(message, input) {
+    let findUser;
     if(message.mentions.users.first()) {
-        return message.mentions.users.first();
+        findUser = message.mentions.users.first();
+        return findUser;
     } else {
         let findUser = await message.guild.members.fetch(input).catch(err => {});
         if(!findUser) {
