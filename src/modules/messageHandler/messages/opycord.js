9c2b41183314d62e512bd7a4451bf1ea524daad2
opysium <73075946+opysium@users.noreply.github.com>
2021-03-04 18:32:42 +0300

    Changed randomiser

--- src/modules/messageHandler/messages/opycord.js
+++ src/modules/messageHandler/messages/opycord.js
@@ -1,9 +1,10 @@
+const random = require('random');
 const randomMessages = ['At your service, sir.', 'Yes, sir.', 'I\'m busy.', 'Fuck off.', '?'];
 
 module.exports = {
     name: 'opycord',
     aliases: [],
     async execute(message, client) {
-        message.channel.send(randomMessages[Math.floor(Math.random() * Math.floor(randomMessages.length))]);
+        message.channel.send(randomMessages[random.int(0, randomMessages.length)]);
     }
 };
